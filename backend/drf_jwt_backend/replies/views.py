5e42d06f799ecce19768f5b7a184d65cd7e0e360
StephanieCobble <cobble.stephanie@gmail.com>
2022-04-13 13:16:38 -0700

    views updated for the replies. Added new class to views for fk

--- backend/drf_jwt_backend/replies/views.py
+++ backend/drf_jwt_backend/replies/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from itertools import product
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Replies
+from .serializer import repliesSerializer
 
 # Create your views here.
+
+class RepliesList(APIView, AllowAny):
+    def get(self, request, format=None):
+        replies = Replies.objects.all()
+        serializer = repliesSerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = repliesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class RepliesDetail(APIView, IsAuthenticated):
+    def get_object(self, pk):
+        try:
+            return Replies.objects.get(pk=pk)  
+        except Replies.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        replies = self.get_object(pk)
+        serializer = repliesSerializer(replies)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        replies = self.get_object(pk)
+        serializer = repliesSerializer(replies, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk, format=None):
+        replies = self.get_object(pk)
+        replies.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RepliesFK(APIView, AllowAny):
+    def get(self, request, fk, format=None):
+        replies = Replies.objects.filter(comments=fk)
+        serializer = repliesSerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
