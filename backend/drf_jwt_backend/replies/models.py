1b976b7d8c142d23347dfa9c16744cfa942e0f99
StephanieCobble <cobble.stephanie@gmail.com>
2022-04-13 13:42:30 -0700

    adjusted the replies model file so now the backend is able to talk to the database

--- backend/drf_jwt_backend/replies/models.py
+++ backend/drf_jwt_backend/replies/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from comments.models import Comments
 # Create your models here.
 
-class Replies(models.model):
+class Replies(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(to=Comments, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
