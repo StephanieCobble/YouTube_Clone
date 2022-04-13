b181a513a0c6a586b58846287b0bff601a4e289c
StephanieCobble <cobble.stephanie@gmail.com>
2022-04-13 13:56:01 -0700

    added urls for each app and updated setting to reflect new urls

---
+++ backend/drf_jwt_backend/replies/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns 
+from .import views
+
+urlpatterns = [
+    path('', views.RepliesList.as_view()),
+    path('<int:pk>/', views.RepliesDetail.as_view()),
+    path('fk/<int:fk>/', views.RepliesFK.as_view())
+]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
