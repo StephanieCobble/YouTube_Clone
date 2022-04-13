0fb26b13bfc62bace11e0883500dae2d6bc111e4
codi kingsbury <codikingsbury117@gmail.com>
2022-04-13 12:47:17 -0700

    models for comments/replies comleted, serializer started

--- backend/drf_jwt_backend/replies/models.py
+++ backend/drf_jwt_backend/replies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Replies(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
---
+++ backend/drf_jwt_backend/replies/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Replies;
+
+
+class repliesSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Replies
+        fields = ['']
